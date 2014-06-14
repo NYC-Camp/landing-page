49bee9c2aeb57f6befb1b0f37de78ff4ecdebdb6
Kris Brandow <kris.brandow@gmail.com>
2014-06-14 18:10:48 -0400

    Adding a redirect for any page that is not the 'nyccamp.org', redirects to 2014.nyccamp.org/{path}.

---
+++ js/2014redirect.js
@@ -0,0 +1,8 @@
+(function($) {
+    $(document).ready(function() {
+    var path = location.pathname;
+    if (path != "/") {
+        window.location.replace("http://2014.nyccamp.org/" + path)
+    }
+    })
+})(jQuery)
