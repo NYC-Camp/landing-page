5975cb7e5e05485dd041a04dab27a8c7bf6a4397
Kris Brandow <kris.brandow@gmail.com>
2014-06-14 18:26:21 -0400

    Removing extraneous forward slash from the redirected URLs.

--- js/2014redirect.js
+++ js/2014redirect.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
     var path = location.pathname;
     if (path != "/") {
-        window.location.replace("http://2014.nyccamp.org/" + path)
+        window.location.replace("http://2014.nyccamp.org" + path)
     }
     })
 })(jQuery)
